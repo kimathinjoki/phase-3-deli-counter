f4c12c05185a2210271b8bb0f7f3a7f8f59c6274
kimathinjoki <dfkimathinjoki@gmail.com>
2023-02-07 22:49:24 -0800

    to be completed

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(katz_deli= [])
+    
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+   else
+    line= katz_deli.map.with_index(1) do |b,index|
+        "#{index}. #{b}" 
+        end
+        puts "The line is currently: " + line.join(' ')
+        # puts line
+    end     
+end
+
+   line(["bill", "kelly", "Liz"])
+
+
+def take_a_number(katz_deli=[], name)
+    new_line = katz_deli << name
+    puts "Welcome, #{name}. You are number #{new_line.index(name)+1} in line."
+end
+
+take_a_number(["kim", "liz", "rot"], "bob")
+
+
